2795d5c88bdb363735d4e291a556831f2e1a3c26
NiamNjam <nida.kirdulyte@gmail.com>
2024-04-17 20:02:13 +0300

    trail and particles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject landParticles;
     public float speed = 5;
     Rigidbody2D rb;
     Vector2 input = Vector2.left;
     public bool canDash;
     public LayerMask wallLayer;
-    public GameObject coin;
     public AudioSource audio;
+    public AudioSource audio1;
+    public AudioSource audio2;
     public AudioClip clip;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    bool hasLanded;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,21 +26,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        /*
         var hit = Physics2D.Raycast(transform.position, input, 1f, wallLayer);
         canDash = hit.collider != null && hit.collider.CompareTag("Wall");
+        */
 
         var newInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if (canDash && newInput != Vector2.zero)
+        if (rb.velocity.magnitude < 0.1f && newInput != Vector2.zero) //velocity - xyz rodykle
         {
             input = newInput;
+            transform.up = -input;
+            /*
             //convert input to z angle
             var angle = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, 0, angle + 90);
+            */
         }
 
         rb.velocity = speed * input;
-
     }
     
     void OnTriggerEnter2D(Collider2D collision)
@@ -47,6 +55,22 @@ public class Player : MonoBehaviour
             
             Destroy(collision.gameObject);
         }
+        if (collision.gameObject.CompareTag("BigCoin"))
+        {
+            audio1.clip = clip1;
+            audio1.Play();
+            Destroy(collision.gameObject);
+           
+        }
+        if (collision.gameObject.CompareTag("Star"))
+        {
+            audio2.clip = clip2;
+            audio2.Play();
+            Destroy(collision.gameObject);
+
+        }
     }
+
     
+
 }
